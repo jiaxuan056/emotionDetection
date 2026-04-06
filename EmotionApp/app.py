9a56ed7136709f8bfbd158f526b589d889897121
jiaxuan056 <163286298+jiaxuan056@users.noreply.github.com>
2026-04-06 22:17:56 +0800

    Update app.py

--- EmotionApp/app.py
+++ EmotionApp/app.py
@@ -8,15 +8,14 @@ import numpy as np
 import os
 from PIL import Image
 from skimage.feature import hog
-
 # -------------------------
 # 1️⃣ Download Models from Google Drive
 # -------------------------
 CNN_FILE_ID = "1QVCR7tt7NFmZvMYkphjH3eOsVaaDgD7w"
-SVM_FILE_ID = "1GWQR8acH4uycRAAupBAOVtLMnEtzBrfK"
+SVM_FILE_ID = "1c94crk9dwSyabPI1neObqLZeNJTbo5_M"
 
 CNN_FILE_NAME = "best_CNNModel.h5"
-SVM_FILE_NAME = "svm_emotion_model.joblib"
+SVM_FILE_NAME = "svm_hog_lbp_model.joblib"
 
 @st.cache_resource(show_spinner=True)
 def load_models():
